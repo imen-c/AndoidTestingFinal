0e43ed489c5844a4b1d57e1d50566b8face26623
imen <imen.cheref@letsgocity.be>
2023-01-30 12:27:56 +0100

    tabLayout adapter + viewBinding

--- app/src/main/java/com/example/androidtestingfinal/MainActivity.kt
+++ app/src/main/java/com/example/androidtestingfinal/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.androidtestingfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.example.androidtestingfinal.adapter.TabLayoutAdapter
+import com.example.androidtestingfinal.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        initTabLayout()
+    }
+
+    private fun initTabLayout(){
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+        val adapter = TabLayoutAdapter(supportFragmentManager)
+
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
     }
 
 
--- app/src/main/java/com/example/androidtestingfinal/adapter/TabLayoutAdapter.kt
+++ app/src/main/java/com/example/androidtestingfinal/adapter/TabLayoutAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.androidtestingfinal.adapter
 
-class TabLayoutAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.androidtestingfinal.fragment.Fragment1
+import com.example.androidtestingfinal.fragment.Fragment2
+import com.example.androidtestingfinal.fragment.Fragment3
+
+class TabLayoutAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> Fragment1()
+            1 -> Fragment2()
+            else -> Fragment3()
+        }
+    }
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            0 -> "Tab 1"
+            1 -> "Tab 2"
+            else -> "Tab 3"
+        }
+    }
 }
